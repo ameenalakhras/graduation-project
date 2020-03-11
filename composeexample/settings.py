66ad9f75ace97456f194b458ce60f7537e571b19
aminalakhras <amin603123@gmail.com>
2020-03-11 20:24:38 +0200

    change aws s3 settings to environment variables, reorder the code in the settings file, add to do list

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -12,40 +12,33 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-# from django.contrib.auth.models import User
-# heroku or local
-database_status = "global"
-# heroku or local
-server_status = "local"
+
+
 WEBSITE_NAME = "aug-classroom"
-WEBSITE_GLOBAL_URL = f'{WEBSITE_NAME}.herokuapp.com'
-USE_S3 = False
-USE_AWS_FOR_OFFLINE_USAGE=True
-DEBUG = True
 # the name for the directory that the settings.py file is in
 PROJECT_MAIN_APP_NAME="composeexample"
-# database_status = None
+AUTH_USER_MODEL = "authentication.User"
+
+WEBSITE_GLOBAL_URL = f'{WEBSITE_NAME}.herokuapp.com'
+
+# global or local
+database_status = os.getenv("DATABASE_STATUS")
+# global or local
+server_status = os.getenv("SERVER_STATUS")
+
+# convert string to boolean
+USE_S3 = (os.getenv("USE_S3") == "True")
+USE_AWS_FOR_OFFLINE_USAGE = (os.getenv("USE_AWS_FOR_OFFLINE_USAGE") == "True")
+DEBUG = (os.getenv("DEBUG") == "True")
 
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-import dj_database_url
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=os.getenv("DJANGO_SECRET_KEY")
-
-
-ALLOWED_HOSTS = []
-
-AUTH_USER_MODEL = "authentication.User"
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
