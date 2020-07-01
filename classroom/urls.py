0979ff2d66ce3371541bb0f90891f846225fce66
ameenalakhras <amin603123@gmail.com>
2020-07-01 22:29:11 +0300

    change the classroom api routes

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 
 from classroom.views import ClassRoomViewSet, CommentViewSet, TaskViewSet, PostViewSet, MaterialViewSet#, ClassRoomTeacherViewSet
-
+list_create = {"get": "list", "post": "create"}
+all_actions = {"get": "retrieve", "delete": "destroy", "post": "create", "put": "partial_update"}
 basic_actions = {"get": "retrieve", "delete": "destroy", "post": "create"}
 retrieve_destroy = {"get": "retrieve", "delete": "destroy"}
 
 urlpatterns = [
-    path('classroom/', ClassRoomViewSet.as_view(actions={"get": "list", "post":"create"}), name="classroom_main"),
-    path('classroom/pk/<int:pk>', ClassRoomViewSet.as_view(actions=retrieve_destroy), name="classroom_detail"),
-    path('classroom/pc/<slug:promo_code>', ClassRoomViewSet.as_view(actions={"post": "enroll"}), name="classroom_enroll"),
+    path('classroom/', ClassRoomViewSet.as_view(actions=list_create), name="classroom_main"),
+    path('classroom/<int:pk>', ClassRoomViewSet.as_view(actions=retrieve_destroy),
+         name="classroom_detail"
+         ),
+    path('classroom/<slug:promo_code>/enroll',
+         ClassRoomViewSet.as_view(actions={"post": "enroll"}), name="classroom_enroll"
+         ),
+
     path('comment/', CommentViewSet.as_view(actions=basic_actions), name="comment"),
     path('task/', TaskViewSet.as_view(actions=basic_actions), name="task"),
     path('post', PostViewSet.as_view(actions=basic_actions), name="post"),
