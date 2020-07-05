7f6a0bce691110d336e11a3c2e288b2cc33c40c7
ameenalakhras <amin603123@gmail.com>
2020-07-06 00:47:23 +0300

    add taskSolutionInfo to the admin dashboard

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from classroom.models import ClassRoom, Post, Comments, Material, \
-                             TaskSolution, Task, TaskSOlutionInfo
+                             TaskSolution, Task, TaskSolutionInfo
 
 admin.site.register(ClassRoom)
 admin.site.register(Post)
 admin.site.register(TaskSolution)
 admin.site.register(Task)
-admin.site.register(TaskSOlutionInfo)
+admin.site.register(TaskSolutionInfo)
 admin.site.register(Comments)
 admin.site.register(Material)
