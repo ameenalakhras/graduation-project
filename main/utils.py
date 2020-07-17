70f0e74cdddc2b5ded6c9a83f67565605617f2a3
ameenalakhras <amin603123@gmail.com>
2020-07-17 05:29:38 +0300

    fix bugs in file creation

--- main/utils.py
+++ main/utils.py
@@ -18,10 +18,11 @@ def get_full_user_path(instance):
 def get_avatar_path(instance, filename):
     """upload the avatar image and put the user name in the bath of the image """
     random_id = generate_string_random_id()
-    user_path = get_full_user_path()
+    user_path = get_full_user_path(instance)
     return f'{user_path}/avatar/{random_id}/{filename}'
 
 def get_attchment_path(instance, filename):
     """return the attachemnt path that it should be in"""
     random_id = generate_string_random_id()
+    user_path = get_full_user_path(instance)
     return f'{user_path}/attachments/{random_id}/{filename}'
