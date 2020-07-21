e271b2e3038f85627adee32d7f05ae652eb34ed5
ameenalakhras <amin603123@gmail.com>
2020-07-22 02:40:35 +0300

    trying to fix that images doesn't upload to aws

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -225,7 +225,7 @@ DEFAULT_USER_PATH = os.path.join(DEFAULT_PATH, 'users')
 # DEFAULT_PROJECT_AVATAR_PATH = os.path.join(DEFAULT_PROJECT_PATH, "avatar/index.png")
 
 # aws s3 settings
-if USE_S3:
+if USE_S3 or USE_AWS_FOR_OFFLINE_USAGE:
     # aws settings
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -236,32 +236,23 @@ if USE_S3:
     # s3 static settings
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-else:
-
-    if USE_AWS_FOR_OFFLINE_USAGE:
-
-        AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-        AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-        AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-        AWS_DEFAULT_ACL = 'public-read'
-        AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-        AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-        # s3 static settings
-        STATIC_LOCATION = 'static'
-        STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    # s3 private media settings
+    PRIVATE_MEDIA_LOCATION = 'private'
+    if USE_S3:
+        STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    else:
         STATICFILES_STORAGE = f'{PROJECT_MAIN_APP_NAME}.storage_backends.StaticStorage'
         # s3 public media settings
-        PUBLIC_MEDIA_LOCATION = 'media'
-        MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
         DEFAULT_FILE_STORAGE = f'{PROJECT_MAIN_APP_NAME}.storage_backends.PublicMediaStorage'
         # s3 private media settings
-        PRIVATE_MEDIA_LOCATION = 'private'
         PRIVATE_FILE_STORAGE = f'{PROJECT_MAIN_APP_NAME}.storage_backends.PrivateMediaStorage'
-    else:
-        STATIC_URL = '/static/'
-        STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-        MEDIA_URL = '/mediafiles/'
-        MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_URL = '/mediafiles/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
