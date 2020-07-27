7b264db29c1f2929f4e34e57a12558539ab24f09
ameenalakhras <amin603123@gmail.com>
2020-07-27 07:38:28 +0300

    add and create tables for 'fcm token request & notifications' requests, order requests by created-date

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from authentication.models import User, CustomToken
+from authentication.models import User, CustomToken, FCMToken, PushMessages
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(CustomToken)
+admin.site.register(FCMToken)
+admin.site.register(PushMessages)
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from authentication.views import UserRegistrationAPIView, UserLoginAPIView, UserTokenAPIView, UserPasswordChange, \
-    CustomTokenCreateAPIView, CustomTokenRetrieveDestroyUpdateAPIView
+    CustomTokenCreateAPIView, CustomTokenRetrieveDestroyUpdateAPIView, FCMTokenCreateAPIView, \
+    FCMTokenUpdateAPIView, PushMessagesListApiView
 
 app_name = 'authentication'
 
@@ -11,4 +12,7 @@ urlpatterns = [
     path('password_change/', UserPasswordChange.as_view(), name="password_change"),
     path("forgot_password/", CustomTokenCreateAPIView.as_view(), name="forgot_password"),
     path("reset_password/", CustomTokenRetrieveDestroyUpdateAPIView.as_view(), name="reset_password"),
+    path("fcm_token/", FCMTokenCreateAPIView.as_view(), name="fcm_token_create"),
+    path("fcm_token/<int:pk>", FCMTokenUpdateAPIView.as_view(), name="fcm_token_update"),
+    path("notifications/", PushMessagesListApiView.as_view(), name="push_notifications")
 ]
