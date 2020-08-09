42c47d7b5394027b054a7f6470905b4b6831eec9
ameenalakhras <amin603123@gmail.com>
2020-08-09 03:19:47 +0300

    add youtube api to media requests to retrieve the video name, description and thumbnail image

--- course/models.py
+++ course/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
 
 from classroom.utils import default_avatar_img
 from course.choices import PROVIDER_CHOICES
+from course.utils import create_video_details
 from main.models import BaseModel
 from django.conf import settings
 from classroom.models import ClassRoom
-from main.utils import get_avatar_path
+from main.utils import get_avatar_path, get_thumbnail_path
 
 
 class Course(BaseModel):
@@ -16,10 +18,15 @@ class Course(BaseModel):
 
 
 class Media(BaseModel):
+    title = models.CharField(max_length=300, null=True)
+    description = models.TextField(max_length=5000, null=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="media")
     provider = models.CharField(choices=PROVIDER_CHOICES, default=PROVIDER_CHOICES[0][0], max_length=30)
     path = models.URLField()
     thumbnail = models.ImageField(
-        upload_to=get_avatar_path, default=default_avatar_img(),
+        upload_to=get_thumbnail_path, default=default_avatar_img(),
         max_length=1000, null=True
     )
+
+
+pre_save.connect(create_video_details, sender=Media)
